0f4d5dd0551752e1f45a852a2d2bc3b44c98c43f
an <dangthanhan507@gmail.com>
2022-08-04 13:12:35 -0700

    Setting up MPC Linear/Convex Optimization

--- ChaserMPC.m
+++ ChaserMPC.m
@@ -1,6 +1,31 @@
 classdef ChaserMPC
     methods (Static)
-        function us = optimizeLinear(traj0, traj_horizon, Q, R, n_horizon, phase)
+        function [A,B] = linearDynamics(T)
+            mu_GM = 398600.4; %km^2/s^2;
+            R = ARPOD_Benchmark.a;
+
+            n = sqrt(mu_GM / (R.^3) );
+            A = zeros(6,6);
+            B = zeros(6,3);
+            S = sin(n * T);
+            C = cos(n * T);
+
+            A(1,:) = [4-3*C,0,0,S/n,2*(1-C)/n,0];
+            A(2,:) = [6*(S-n*T),1,0,-2*(1-C)/n,(4*S-3*n*T)/n,0];
+            A(3,:) = [0,0,C,0,0,S/n];
+            A(4,:) = [3*n*S,0,0,C,2*S,0];
+            A(5,:) = [-6*n*(1-C),0,0,-2*S,4*C-3,0];
+            A(6,:) = [0,0,-n*S,0,0,C];
+
+            B(1,:) = [(1-C)/(n*n),(2*n*T-2*S)/(n*n),0];
+            B(2,:) = [-(2*n*T-2*S)/(n*n),(4*(1-C)/(n*n))-(3*T*T/2),0];
+            B(3,:) = [0,0,(1-C)/(n*n)];
+            B(4,:) = [S/n,2*(1-C)/n, 0];
+            B(5,:) = [-2*(1-C)/n,(4*S/n) - (3*T),0];
+            B(6,:) = [0,0,S/n];
+
+        end
+        function [xs,us] = optimizeLinear(traj0, traj_horizon, Q, R, n_horizon, timestep, phase)
             %{
                 min x^T*Q*x + u^T*R*u
                     s.t. x_k+1 = Ax_k + Bu_k
@@ -13,70 +38,109 @@ classdef ChaserMPC
                          [xdot,ydot,zdot] <= Vbar
                          -[xdot,ydot,zdot[ <= Vbar 
             %}
-            % n_horizon is for the states and n_horizon-1 is for control inputs.
-            names = cell(1,9*n_horizon-3);
-            
-            %modelling the objective function
-            quadratic_Q = zeros(9*n_horizon-3);
-            for i = 1:6:6*n_horizon
-                %add all names to gurobi
-                names(i) = {"x"+i};
-                names(i+1) = {"y"+i};
-                names(i+2) = {"z"+i};
-                names(i+3) = {"xdot"+i};
-                names(i+4) = {"ydot"+i};
-                names(i+5) = {"zdot"+i};
+            [A,B] = ChaserMPC.linearDynamics(timestep);
 
-                quadratic_Q(i) = Q(1,1);
-                quadratic_Q(i+2) = Q(2,2);
-                quadratic_Q(i+3) = Q(3,3);
-                quadratic_Q(i+4) = Q(4,4);
-                quadratic_Q(i+5) = Q(5,5);
-                quadratic_Q(i+6) = Q(6,6);
+            %setting up H matrix (combined Q and R)
+            H = [];
+            for i = 1:n_horizon
+                H = [H, diag(Q).'];
+                if i ~= n_horizon
+                    H = [H, diag(R).'];
+                end
             end
-            for i = 6*n_horizon+1:3:9*n_horizon-3
-                names(i) = {"ux"+i};
-                names(i+1) = {"uy"+i};
-                names(i+2) = {"uz"+i};
+            ndim = length(H);
+            f = zeros(ndim,1);
 
-                quadratic_Q(i) = R(1,1);
-                quadratic_Q(i+1) = R(2,2);
-                quadratic_Q(i+2) = R(3,3);
-            end
+            H = sparse(diag(H));
 
-            if (phase == 1 || phase == 2 || phase == 4)
-                %setting up constraints for rendezvous
-                % dynamics constraints = # of control inputs
-                % x0 = xbar0 => 1 constraints
-                % make sure all u's are less than ubar => 2*(# control inputs)
-                constr_dim = n_horizon - 1 + n_horizon-1 + 1 + 2*(n_horizon-1);
-                constraints = zeros(constr_dim,9*n_horizon-3);
-            else
-                %setting up constraints for docking
-                % make sure all x's are less than vbar => 2*(# x inputs)
-                % linear LOS constraints => # x inputs
-                constr_dim = n_horizon-1 + n_horizon-1 + 1 + 2*(n_horizon-1) + 2*n_horizon + n_horizon;
-                constraints = zeros(constr_dim, 9*n_horizon-3);
-            end
+            % H represents the condensed quadratic version of 
+            % sum_1->N x^T*Q*x + u^T*R*u
 
+            %constructing equality matrix
+            Aeq = [];
+            beq = zeros(9*n_horizon-3,1);
             for i = 1:n_horizon-1
-                %adding constraints 
-                % Ax_k + Bu_k = x_k+1
-                location_x = 6*(i-1)+1;
-                constraints(i,location_x:location_x+5) = 1;
-                location_u = 6*n_horizon + 3*i + 1;
-                constraints(i,location_u:location_u+2) = 1; 
 
+                row = [];
+                for j = 1:n_horizon
+                    if j == i
+                        row = [row, A,B];
+                    elseif j == i+1
+                        row = [row, -eye(6), zeros(6,3)];
+                    elseif j == n_horizon
+                        row = [row, zeros(6,6)];
+                    else
+                        row = [row, zeros(6,6), zeros(6,3)];
+                    end
+                end
+
+                Aeq = [Aeq;row];
+            end
+            
+            %last iteration, n_horizon for the last equality constraint,
+            %x0 = xbar0
+            row = [];
+            for i = 1:n_horizon-1
+                if i == 1
+                    row = [row, eye(6), zeros(6,3)];
+                else
+                    row = [row, zeros(6,6), zeros(6,3)];
+                end
+            end
+            Aeq = [Aeq;row];
+            beq(9*n_horizon-5:9*n_horizon) = traj0;
+
+            %setting up bound constraints
+            ubar = ARPOD_Benchmark.ubar;
+            ub = [];
+            lb = [];
+            for i = 1:n_horizon-1
+                ub = [ub; inf;inf;inf;inf;inf;inf];
+                ub = [ub; ubar;ubar;ubar];
+            end
+            ub = [ub; inf;inf;inf;inf;inf;inf];
+            lb = -ub;
+
+            % ig it's phase 3, add in the LOS constraint and vbar
+            % constraint
+            vbar = ARPOD_Benchmark.Vbar;
+            A = [];
+            b = zeros(4*n_horizon);
+            if phase == 3
+                %LOS Matrix
+                theta1 = ARPOD_Benchmark.theta * pi / 180;
+                theta2 = theta1;
+                LOS = [ sin(theta1/2), cos(theta1/2), 0; 
+                        sin(theta1/2), -cos(theta1/2), 0;
+                        sin(theta2/2), 0, cos(theta2/2);
+                        sin(theta2/2), 0, -cos(theta2/2)];
+                LOS = [LOS, zeros(4,3)];
+
+                for i = 1:9:9*n_horizon-3
+                    %vbar constraints
+                    ub(i+3:i+5) = [vbar;vbar;vbar];
+                    lb(i+3:i+5) = [-vbar;-vbar;-vbar];
+                    
+                    A = [A; LOS];
+                end
             end
 
-            constraints(constr_dim,1) = 1;
 
+            % in case this doesn't work, add in the traj_horizon as the
+            % initial input.
+            x = quadprog(H,f,A,b,Aeq,beq,lb,ub, traj_horizon);
 
-            model.varnames = names;
-            model.Q = sparse(quadratic_Q);
-            model.modelsense = 'min';
-
-
+            xs = [];
+            us = [];
+            for i = 1:n_horizon
+                x_id = 9*(i-1)+1;
+                xs = [xs, x(x_id:x_id+5)];
+                if i ~= n_horizon
+                    u_id = 9*(i-1)+1+6;
+                    us = [us, x(u_id:u_id+2)];
+                end
+            end
+            % return xs and us
         end
         function us = optimizeNonlinear()
         end
